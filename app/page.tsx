c0efafab57a9b14e49a8a5bfba4473b2c5128599
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-07-14 05:00:40 -0300

    Fix: acertos iniciais

--- app/page.tsx
+++ app/page.tsx
@@ -12,26 +12,30 @@ import {
 } from '@/components/ui/card';
 
 export default async function Component() {
-  return (
-    <main className="container px-1 py-10 md:p-10">
-      <Card>
-        <CardHeader className="px-7">
-          <CardTitle>Pedidos</CardTitle>
-          <CardDescription>
-            Uma listagem de pedidos do seu negócio.
-          </CardDescription>
-          <div className="flex pt-10 gap-4">
-            <SearchInput />
-            <FilterDropdown />
-          </div>
-        </CardHeader>
-        <CardContent>
-          <OrdersTable />
-          <div className="mt-8">
-            <Pagination />
-          </div>
-        </CardContent>
-      </Card>
-    </main>
-  );
+    return (
+        <main className="container px-1 py-10 md:p-10">
+            <Card>
+                <CardHeader className="px-7">
+                    <CardTitle>Pedidos</CardTitle>
+
+                    <CardDescription>
+                        Uma listagem de pedidos do seu negócio.
+                    </CardDescription>
+
+                    <div className="flex pt-10 gap-4">
+                        <SearchInput />
+                        <FilterDropdown />
+                    </div>
+                </CardHeader>
+
+                <CardContent>
+                    <OrdersTable />
+                    
+                    <div className="mt-8">
+                        <Pagination />
+                    </div>
+                </CardContent>
+            </Card>
+        </main>
+    );
 }
