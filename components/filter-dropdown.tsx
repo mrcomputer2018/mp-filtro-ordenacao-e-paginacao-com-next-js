f6d9b24d1924d603fd8c9f78fb9a0924f9b8b3bd
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-03-16 07:24:07 -0300

    feat: Add filter functionality to dropdown component with state management

--- components/filter-dropdown.tsx
+++ components/filter-dropdown.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import {
   DropdownMenu,
@@ -11,34 +12,48 @@ import {
 
 import { Button } from '@/components/ui/button';
 import { Filter } from 'lucide-react';
+import { useState } from 'react';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function FilterDropdown() {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size={'default'}
-          className="flex gap-2 text-slate-600"
-        >
-          <Filter className="h-4 w-4" />
-          Status
-        </Button>
-      </DropdownMenuTrigger>
 
-      <DropdownMenuContent className="w-16">
-        <DropdownMenuLabel>Filtrar por:</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value="">
-          <DropdownMenuRadioItem value="">Todos</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="pending">
-            Pendente
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="completed">
-            Completo
-          </DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+    const [filterStatus, setFilterStatus] = useState('');
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    const { replace } = useRouter();
+
+    function handleChangeFilterStatus(value: string) {
+        setFilterStatus(value);
+    }
+    
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                variant="outline"
+                size={'default'}
+                className="flex gap-2 text-slate-600"
+                >
+                <Filter className="h-4 w-4" />
+                    Status
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="w-16">
+                <DropdownMenuLabel>Filtrar por:</DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuRadioGroup value={filterStatus} onValueChange={handleChangeFilterStatus}>
+                    <DropdownMenuRadioItem value="">Todos</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="pending">
+                        Pendente
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="completed">
+                        Completo
+                    </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 }
